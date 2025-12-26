b7a3ee462a261dcce69e72872bf2221a8d383387
marker <j@saro.me>
2025-12-26 20:29:03 +0900

    역직렬화 설명 추가

--- src/main/kotlin/anissia/infrastructure/configuration/JacksonConfiguration.kt
+++ src/main/kotlin/anissia/infrastructure/configuration/JacksonConfiguration.kt
@@ -10,7 +10,14 @@ import tools.jackson.module.kotlin.KotlinModule
 
 @Configuration
 class JacksonConfiguration {
-    // kotlin에서 jackson 사용시 object 만들기에 부족한 값이 있더라도 null이 아닌 기본값을 쓴다.
+    /**
+     * jackson 3.x(추정) 이후 역직렬화의 기본값이 변경되어 이전 값과 유사하게 동작하도록 설정
+     *
+     * 조건: 역직렬화시 맴버에 필요한 파라미터가 없는경우.
+     * - jackson 구버전: 기본값 사용.
+     * - jackson 신버전: null 사용. (해당 맴버가 nullable이 아닌 경우 코틀린 null 오류발생)
+     * (신버전의 정책이 프로그래밍 원칙상 더 올바른 방향이긴 하나 현 프로젝트의 호환성과 잠재적 버그 때문에 구버전과 유사하게 수정.)
+     */
     @Bean
     fun objectMapper(): ObjectMapper {
         return JsonMapper.builder()
