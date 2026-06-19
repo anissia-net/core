afbd0de551e8ee463ccf4d963603dab1d8797497
marker <j@saro.me>
2026-06-19 21:29:24 +0900

    DAT 4.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
 	// lib
 	implementation("me.saro:kit:0.2.3")
 	implementation("org.mindrot:jbcrypt:0.4")
-	implementation("me.saro:dat:4.2.0")
+	implementation("me.saro:dat:4.2.2")
     implementation("tools.jackson.module:jackson-module-kotlin")
 
 	// logger
